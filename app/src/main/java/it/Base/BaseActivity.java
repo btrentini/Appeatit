ab5901e8bd351edd74813986ac25b9d45b68d46d
Tobias <tobias.guimaraes02@gmail.com>
2017-05-01 14:29:45 -0300

    Facebook integration almost done

--- app/src/main/java/it/Base/BaseActivity.java
+++ app/src/main/java/it/Base/BaseActivity.java
@@ -2,18 +2,26 @@ package it.Base;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialize.util.UIUtils;
 
+import it.appeatit.LoginActivity;
 import it.appeatit.R;
 
 /**
@@ -35,7 +43,8 @@ public class BaseActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    public void setupNavigationDrawer(Activity activity){
+    public void setupNavigationDrawer(final Activity activity){
+
         new DrawerBuilder().withActivity(activity).build();
         //if you want to update the items at a later time it is recommended to keep it in a variable
         PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Home");
@@ -43,8 +52,23 @@ public class BaseActivity extends AppCompatActivity {
 
 
         //create the drawer and remember the `Drawer` result object
+
+
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withCompactStyle(true)
+                .withTranslucentStatusBar(true)
+                .withSelectionListEnabledForSingleProfile(false)
+                .withHeaderBackground(R.color.primary)
+                .withTextColor(Color.BLACK)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Appeatit").withIcon(getResources().getDrawable(R.mipmap.ic_launcher))
+                )
+                .build();
+
         Drawer result = new DrawerBuilder()
                 .withActivity(this)
+                .withAccountHeader(headerResult)
                 .withToolbar(toolbar)
                 .addDrawerItems(
                         item1,
@@ -55,8 +79,10 @@ public class BaseActivity extends AppCompatActivity {
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        if(position == 2){
-
+                        if(position == 3){
+                            Intent intent = new Intent();
+                            intent.setClass(activity, LoginActivity.class);
+                            startActivity(intent);
                         }
                         return true;
                     }
---
+++ app/src/main/java/it/appeatit/LoginActivity.java
@@ -0,0 +1,114 @@
+package it.appeatit;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setTitle(R.string.attention);
+                builder.setMessage(R.string.login_soon);
+                builder.setPositiveButton(R.string.ok, null);
+                builder.show();
+            }
+        });
+
+        /**
+         * Facebook login
+         * */
+
+
+        if(AccessToken.getCurrentAccessToken() != null){
+            Log.d("DEBUG","LOGADO");
+            AccessToken ac = AccessToken.getCurrentAccessToken();
+
+        }else {
+            Log.d("DEBUG","FAZ LOGIN");
+            callbackManager = CallbackManager.Factory.create();
+            LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+            loginButton.setReadPermissions(Arrays.asList(
+                    "public_profile", "email"));
+
+            // Other app specific specialization
+
+            // Callback registration
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    // App code
+                    GraphRequest request = GraphRequest.newMeRequest(
+                            loginResult.getAccessToken(),
+                            new GraphRequest.GraphJSONObjectCallback() {
+                                @Override
+                                public void onCompleted(JSONObject object, GraphResponse response) {
+                                    try {
+                                        Log.v("LoginActivity", response.toString());
+
+                                        // Application code
+                                        String email = object.getString("email");
+                                        String birthday = object.getString("birthday"); // 01/31/1980 format
+
+                                    }catch (JSONException e){
+                                        Log.d("DEBUG",e.getMessage());
+                                    }
+                                }
+                            });
+                    Bundle parameters = new Bundle();
+                    parameters.putString("fields", "id,name,email,gender,birthday");
+                    request.setParameters(parameters);
+                    request.executeAsync();
+                }
+
+                @Override
+                public void onCancel() {
+                    // App code
+                    Log.d("DEBUG", "Login Canceled");
+                }
+
+                @Override
+                public void onError(FacebookException exception) {
+                    // App code
+                    Log.d("DEBUG", exception.getMessage());
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
