5187e6250c6a846bf09019e96b72322543abcdfb
Tobias <tobias.guimaraes02@gmail.com>
2017-05-01 23:39:53 -0300

    Facebook integration almost done part 2

--- app/src/main/java/it/Base/BaseActivity.java
+++ app/src/main/java/it/Base/BaseActivity.java
@@ -3,6 +3,8 @@ package it.Base;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -20,7 +22,10 @@ import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.mikepenz.materialize.util.UIUtils;
+import com.squareup.picasso.Picasso;
 
+import it.Model.User;
+import it.Utils.Utils;
 import it.appeatit.LoginActivity;
 import it.appeatit.R;
 
@@ -53,6 +58,19 @@ public class BaseActivity extends AppCompatActivity {
 
         //create the drawer and remember the `Drawer` result object
 
+        String name;
+        int drawable = 0;
+
+        if(Utils.getInstance().getLoginUser(getApplicationContext())){
+            User user = Utils.getInstance().getLoginUserInfo(getApplicationContext());
+            name = user.getName();
+            String url = "https://graph.facebook.com/"+user.getStringID()+"/picture?type=large";
+            Picasso.with(getApplicationContext()).load(url);
+        }else {
+            name = "Appeatit";
+            drawable = R.mipmap.ic_launcher;
+        }
+
 
         AccountHeader headerResult = new AccountHeaderBuilder()
                 .withActivity(this)
@@ -62,7 +80,9 @@ public class BaseActivity extends AppCompatActivity {
                 .withHeaderBackground(R.color.primary)
                 .withTextColor(Color.BLACK)
                 .addProfiles(
-                        new ProfileDrawerItem().withName("Appeatit").withIcon(getResources().getDrawable(R.mipmap.ic_launcher))
+                        new ProfileDrawerItem()
+                                .withName(name)
+                                .withIcon(R.mipmap.ic_launcher)
                 )
                 .build();
 
